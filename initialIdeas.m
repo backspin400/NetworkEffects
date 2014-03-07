a5e8087dc74ede18ae02a77a850964ac65e51272
Barry <bleybovich@gmail.com>
2014-03-07 16:17:02 -0500

    seeing group spread

--- initialIdeas.m
+++ initialIdeas.m
@@ -2,16 +2,30 @@
 
 users=1:100;
 rating=.01:.01:1;
-similarities=zeros(1,100);
+similarities=.01:.01:1;
+group=1:100;
+group
 ratingWeight=1;
 similaritiesWeight=1;
-for i=1:100
-similarities(i)=rand;
-end
+totgroups=100;
+
+% for i=1:100
+% similarities(i)=rand;
+% end
 test=zeros(1,100);
 similarities(50)=.50;
 similarities(80)=.80;
+
 for i=1:100
-test(i)=willSayYes(50,80, rating, similarities, ratingWeight, similaritiesWeight);
+    for j=1:100
+        test(i,j)=willSayYes(users(i),users(j), rating, similarities, ratingWeight, similaritiesWeight);
+        if (test(i))
+            group(i)=group(j);
+        else 
+            totgroups=totgroups+1; 
+            group(i)=0;
+        end
+        
+    end
 end
 sum(test)
